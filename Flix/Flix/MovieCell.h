3a5bf162c8d18b3e9bf72425e1abcf83b6571c53
vinayak iyer <vinayakiyer1234@gmail.com>
2022-09-20 16:10:22 -0700

    Flixter Part 1 Uploaded and Running

---
+++ Flix/Flix/MovieCell.h
@@ -0,0 +1,16 @@
+//
+//  MovieCell.h
+//  Flix
+//
+//  Created by Vinayak Iyer on 9/13/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
